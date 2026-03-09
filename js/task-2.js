c5c7cc6eb0970fb851d2c8e6add5c92c755e10d2
masya <marinabondarska2006@gmail.com>
2026-03-09 11:30:22 +0100

    all js

--- js/task-2.js
+++ js/task-2.js
@@ -1,3 +1,26 @@
+class Storage {
+  #items = [ ];
+
+  constructor(arr){
+    this.#items.push(...arr);
+  }
+
+  getItems(){
+    return [...this.#items]; 
+  }
+
+  addItem(newItem){
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove){
+    if(this.#items.indexOf(itemToRemove) != -1)
+        {
+      this.#items.splice(this.#items.indexOf(itemToRemove),1);
+    }
+  }
+}
+
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 
--- js/task-3.js
+++ js/task-3.js
@@ -1,3 +1,31 @@
+class StringBuilder {
+  #value;
+
+  constructor(initialValue){
+    this.#value = initialValue; 
+  }
+
+  getValue(){
+    return this.#value;
+  }
+
+  padEnd(str){
+    this.#value += str;
+    return this; 
+  }
+
+  padStart(str){
+    this.#value = str + this.#value;
+    return this;
+  }
+
+  padBoth(str){
+    this.#value = str + this.#value + str;
+    return this;
+  }
+}
+
+
 const builder = new StringBuilder(".");
 console.log(builder.getValue()); // "."
 builder.padStart("^");
